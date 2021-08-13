752d07206a87a788dcd4deebdd048976ed8d2046
saurav <sauravsinghrauthan@gmail.com>
2021-08-13 18:57:12 +0530

    desktop site ready

--- js/mainDiv.js
+++ js/mainDiv.js
@@ -1,11 +1,11 @@
 var mainDiv = document.querySelector('.main-first')
 var text = document.querySelector('.text-cont')
-var img=document.querySelector('.img-cont')
+var img = document.querySelector('.img-cont')
 
 console.dir(text);
 
 mainDiv.addEventListener('mousemove', () => {
-    text.style.transform='translateX(0)'
-    img.style.transform='translateX(0)'
-    upBut.style.display='none'
+    text.style.transform = 'translateX(0)'
+    img.style.transform = 'translateX(0)'
+    upBut.style.display = 'none'
 })
