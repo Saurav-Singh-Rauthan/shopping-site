f543605cb3db814a4a6c2baf6a5e76c281dce61b
saurav <sauravsinghrauthan@gmail.com>
2021-08-14 13:25:06 +0530

    added sidebar for small screens

--- js/mainDiv.js
+++ js/mainDiv.js
@@ -2,10 +2,9 @@ var mainDiv = document.querySelector('.main-first')
 var text = document.querySelector('.text-cont')
 var img = document.querySelector('.img-cont')
 
-console.dir(text);
+// console.dir(text);
 
 mainDiv.addEventListener('mousemove', () => {
     text.style.transform = 'translateX(0)'
     img.style.transform = 'translateX(0)'
-    upBut.style.display = 'none'
 })
